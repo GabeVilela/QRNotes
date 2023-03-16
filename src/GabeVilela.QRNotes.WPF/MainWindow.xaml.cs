266f4c4dfa9655362b7d382a31bfa530507da943
Gabriel Vilela <49645243+GabeVilela@users.noreply.github.com>
2023-03-16 08:29:28 -0300

    Update MainWindow.xaml.cs

--- src/GabeVilela.QRNotes.WPF/MainWindow.xaml.cs
+++ src/GabeVilela.QRNotes.WPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GabeVilela.QRNotes.WPF.DTO;
+using GabeVilela.QRNotes.WPF.Services;
+using Microsoft.Web.WebView2.Core;
 
 namespace GabeVilela.QRNotes.WPF
 {
@@ -20,18 +24,27 @@ namespace GabeVilela.QRNotes.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IpcHandler ipc;
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeWebView();
-            //this.Visibility = Visibility.Hidden;
         }
 
         private async void InitializeWebView()
         {
             await WebView.EnsureCoreWebView2Async(null);
+
+            WebView.CoreWebView2.WebMessageReceived += ReceiveIpcMessage;
             WebView.CoreWebView2.Settings.IsGeneralAutofillEnabled = false;
             WebView.CoreWebView2.Settings.IsPasswordAutosaveEnabled = false;
+
+            WebView.CoreWebView2.AddHostObjectToScript("qrNotes", new
+            {
+                Version = GetAppVersion()
+            });
+            ipc = new IpcHandler(WebView.CoreWebView2);
 
 #if DEBUG
             if (Environment.GetEnvironmentVariable("ANGULAR_DEV_SERVER") == "true")
@@ -60,5 +73,26 @@ namespace GabeVilela.QRNotes.WPF
         {
             Title = WebView.CoreWebView2.DocumentTitle;
         }
+
+        private async void ReceiveIpcMessage(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            string messageJSON = args.TryGetWebMessageAsString();
+            string msgReply;
+
+            var message = ipc.TryParseMessage(messageJSON);
+
+            msgReply = await ipc.GetReply(message);
+
+            if (msgReply != null)
+            {
+                ipc.PostMessage(msgReply);
+            }
+        }
+
+        private AppVersion GetAppVersion() => new AppVersion()
+        {
+            Assembly = Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+            WebView = WebView.CoreWebView2.Environment.BrowserVersionString
+        };
     }
 }
