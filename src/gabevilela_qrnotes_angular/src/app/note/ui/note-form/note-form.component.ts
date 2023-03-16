61d54aca508767e3160d16e84f3b46e017317551
GabeVilela <gabevilela@outlook.com>
2023-03-15 23:03:03 -0300

    WIP: TAB hotkey improvements on the editor

--- src/gabevilela_qrnotes_angular/src/app/note/ui/note-form/note-form.component.ts
+++ src/gabevilela_qrnotes_angular/src/app/note/ui/note-form/note-form.component.ts
@@ -16,6 +16,9 @@ import { NoteDTO } from '../../data-access/note-dto.interface';
 import { Router } from '@angular/router';
 import { Subscription, tap } from 'rxjs';
 
+// Matches both bullet lists "- " and numbered lists "1. "
+const listItemRegex = new RegExp(/^(\t{0,}(-|\d{1,}\.)\s)/);
+
 @Component({
   selector: 'app-note-form',
   standalone: true,
@@ -158,5 +161,122 @@ export class NoteFormComponent implements OnInit, OnDestroy, OnChanges {
 
   redirectToQR(): void {
     this.router.navigate(['note', 'qrcode', `${this.noteDTO.id}`]);
+  }
+
+  inputHandler(event: any): void{
+    console.log(event);
+    switch (event.key) {
+      case 'Tab':
+        event.preventDefault();
+
+        if (event.shiftKey){
+          this.removeTab(event.target);
+          break;
+        }
+
+        if (this.isCaretOnBulletOrNumberedListItem(event.target)){
+          this.insertTextOnRowStartCaret(event.target, '\t');
+          break;
+        }
+
+        this.insertTextOnCaret(event.target, '\t');
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  private insertTextOnCaret(field: any, text: string): void{
+    const start = field.selectionStart;
+    const end = field.selectionEnd;
+
+    field.value = `${field.value.substring(0, start)}${text}${field.value.substring(end)}`;
+    field.selectionStart = field.selectionEnd = start + 1;
+  }
+
+  private insertTextOnRowStartCaret(field: any, text: string): void{
+    const start = field.selectionStart;
+    const textBefore = field.value.substring(0,start);
+    let indexRowStartLineBreak = textBefore.lastIndexOf('\n');
+    const textAfter = this.getTextAfter(indexRowStartLineBreak,field.value);
+    const rowsBefore = this.getTextBefore(indexRowStartLineBreak + 1, field.value);
+
+    field.value = `${rowsBefore}${text}${textAfter}`;
+    field.selectionStart = field.selectionEnd = start + 1;
+  }
+
+  private isCaretOnBulletOrNumberedListItem(field:any):boolean{
+    const currentRow = this.getTextOnCaretRow(field);
+    return listItemRegex.test(currentRow);
+  }
+
+  private removeTab(field:any):void{
+    const start = field.selectionStart;
+    const value:string = field.value;
+    
+    const breakRowIndex = value.substring(0,start).lastIndexOf('\n');
+    const rowsBefore = value.substring(0,breakRowIndex);
+    const rowStart = value.substring(breakRowIndex,start);
+
+    const tabIndex = rowStart.lastIndexOf('\t');
+    if (tabIndex < 0) return;
+
+    let result = rowsBefore;
+
+    if (tabIndex === 0){
+      result += rowStart.substring(1);
+    }else{
+      result += rowStart.substring(0,tabIndex);
+      result += rowStart.substring(tabIndex + 1);
+    }
+
+    result += value.substring(start);
+
+    field.value = result;
+    field.selectionStart = field.selectionEnd = start - 1;
+  }
+
+  private getTextOnCaretRow(field:any):string{
+    const start = field.selectionStart;
+    const end = field.selectionEnd;
+
+    let textBefore:string = field.value.substring(0,start);
+    let textAfter:string = field.value.substring(end);
+
+    textBefore = this.getTextAfterRightmostValue(textBefore,'\n');
+    textAfter = this.getTextBeforeLeftmostValue(textAfter,'\n');
+
+    return `${textBefore}${textAfter}`;
+  }
+
+  private getTextBeforeRightmostValue(fullText:string, value:string):string{
+    const index = fullText.lastIndexOf(value);
+    return this.getTextBefore(index,fullText);
+  }
+
+  private getTextAfterRightmostValue(fullText:string,value:string):string{
+    const index = fullText.lastIndexOf(value);
+    return this.getTextAfter(index,fullText);
+  }
+
+  private getTextBeforeLeftmostValue(fullText:string, value:string):string{
+    const index = fullText.indexOf(value);
+    return this.getTextBefore(index, fullText);
+  }
+
+  private getTextAfterLeftmostValue(fullText:string, value:string):string{
+    const index = fullText.indexOf(value);
+    return this.getTextAfter(index,fullText);
+  }
+
+  private getTextBefore(index:number,fullText:string):string{
+    if (index === 0) return "";
+    return fullText.substring(0,index);
+  }
+
+  private getTextAfter(index:number,fullText:string):string{
+    if (index + 1 >= fullText.length) return "";
+    return fullText.substring(index + 1);
   }
 }
